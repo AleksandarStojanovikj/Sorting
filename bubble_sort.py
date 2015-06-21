4319d80a4c6f5eb81b5b74d1b613cea5b7b09128
Aleksandar Stojanovikj <aleksandar_sangi@hotmail.com>
2015-06-21 15:48:52 +0200

    Initial project input

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+length = input("Lenght of array: ")
+
+array = []
+for i in range(0, length):
+	new_number = input("#{0} ".format(i))
+	array.append(new_number)
+
+
+# Nadole ti ja resavas zadacata, sortiraj gi site broevi so bubble sort
