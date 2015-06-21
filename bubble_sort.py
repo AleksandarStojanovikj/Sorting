ab2317e3362be08f0da60d8a29ea0f02644314c8
Aleksandar Stojanovikj <aleksandar_sangi@hotmail.com>
2015-06-21 16:19:25 +0200

    Bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,3 +7,11 @@ for i in range(0, length):
 
 
 # Nadole ti ja resavas zadacata, sortiraj gi site broevi so bubble sort
+for x in range(0, length-1):
+	for i in range(0, length-1):
+		if array[i] > array[i+1]:
+			temp = array[i]
+			array[i] = array[i+1]
+			array[i+1] = temp
+
+print array
