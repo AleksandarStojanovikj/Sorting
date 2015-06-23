7ece015c392597d7f1bc1008e682d2ada3172135
Aleksandar Stojanovikj <aleksandar_sangi@hotmail.com>
2015-06-23 15:12:22 +0200

    Insertions sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,3 +5,14 @@ for i in range(0, length):
 	new_number = input("#{0} ".format(i))
 	array.append(new_number)
 
+
+for i in range(1, length):
+        temp = array[i]
+        x = i - 1
+        while (x >= 0) and (array[x] > temp):
+            array[x+1] = array[x]
+            x = x - 1
+        array[x+1] = temp
+
+
+print array
